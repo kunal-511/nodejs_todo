41ad95e6ab9d6dea5ec87d8540dbf6b8d20d486e
kunal Dugar <yoyokvunal@gmail.com>
2024-03-18 15:17:10 +0530

    custom cors diabled

--- app.js
+++ app.js
@@ -16,18 +16,15 @@ app.use(cookieParser());
 //app.use("api/v1/users", userRouter);
 app.use("/users", userRouter); // user router
 app.use("/task", taskRouter); // task router
-// Custom CORS Middleware
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    // Handling preflight request
-    return res.sendStatus(200);
-  }
-  next();
-});
+// CORS Middleware
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // Allow cookies to be sent
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Nice Working");
