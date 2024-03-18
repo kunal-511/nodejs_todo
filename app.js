e065ffdc459743de8e5b13e83fc8b443862bf357
kunal Dugar <yoyokvunal@gmail.com>
2024-03-18 15:42:51 +0530

    headers removed

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow cookies to be sent
   })
 );
